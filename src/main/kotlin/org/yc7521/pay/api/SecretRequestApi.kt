8792cb17e4c72f0da667cb221d206fb7259cdd1f
xieyongcheng <752106133@qq.com>
2023-04-20 17:40:26 +0800

    update des

--- src/main/kotlin/org/yc7521/pay/api/SecretRequestApi.kt
+++ src/main/kotlin/org/yc7521/pay/api/SecretRequestApi.kt
@@ -16,7 +16,7 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/api/request/secret")
-@Tag(name = "/Api/Role Request")
+@Tag(name = "/Api/ApiKey Request")
 class SecretRequestApi(
   private val secretKeyRepository: SecretKeyRepository,
 ) : BaseApi() {
